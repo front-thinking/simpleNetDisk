114df2230b36acef4045725ac3843b1b1d9fc63f
front-thinking <269354528@qq.com>
2015-09-10 17:05:18 +0800

    fix front bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,43 @@
-$(function () {
-    var preSelected ;
-    var mutliSelected;
-    $(".file-list-item").bind('contextmenu', function (e) {
+$(function() {
+    var preSelected;
+    var selected = [];
+    $(".file-list-item").bind('contextmenu', function(e) {
         e.preventDefault();
-        $(this).find("input[type='checkbox']")[0].checked = true;
-        if(preSelected) preSelected.find("input[type='checkbox']")[0].checked = false;
-        preSelected = $(this);
+        if (selected.length > 1 && selected.indexOf($(this).find("input[type='checkbox']")[0]) !== -1) {
+
+        } else {
+            while(selected.length) selected.pop().checked = false;
+            $(this).find("input[type='checkbox']")[0].checked = true;
+            if (preSelected) preSelected.find("input[type='checkbox']")[0].checked = false;
+            preSelected = $(this);
+        }
         $('#mm').menu('show', {
             left: e.pageX,
             top: e.pageY
         });
         return false;
     });
-    
-    $(".select-all").bind("click", function(){
-        var value = $(this).val();
-        console.log(value);
-        $(".select").each(function(){
-            this.chekced = value;
+
+    $(".select-all").bind("click", function() {
+        var that = this;
+        $(".select").each(function() {
+            this.checked = that.checked;
+            selected.push(this);
         });
-    })
-    
-     $(".file-list").bind('contextmenu', function (e) {
+        if (!that.checked) selected = [];
+    });
+    $(".select").bind("click", function() {
+        if (this.checked) selected.push(this);
+        else selected.splice(selected.indexOf(this), 1);
+        console.log(selected);
+    });
+    $(".file-list").bind('contextmenu', function(e) {
         e.preventDefault();
         $('#mm2').menu('show', {
             left: e.pageX,
             top: e.pageY
         });
     });
+
+    // $("div[action='rename'")
 });
